937af7d110280ee662314582161167c4ab5268c4
jensen <jdegrande1@outlook.com>
2022-03-21 09:18:21 -0700

    updated commenting

--- DeGrande_HW5.py
+++ DeGrande_HW5.py
@@ -2,7 +2,7 @@
 # Author: Jensen DeGrande
 # Computational Methods for Data Analysis, AMATH 582
 # Date of creation: 03/09/22
-# Last edited: -- -- --
+# Last edited: 03/13/22
 
 # Problem/Purpose: The purpose of this assignment is to recover an image from limited observations of its pixels. We
 # will consider Rene Magritte's "The Son of Man" and a corrupted variant in order to understand image recovery. Our goal
@@ -29,7 +29,6 @@ import scipy.fftpack as spfft  # for discrete cosine transform
 # transform reconstructs the signal f fromt the DCT(f). The 2D DCT is defined analogously to 2D FFT by successviely
 # applying the 1D DCT to the rows and columns of a 2D image.
 # perform the following on the rescaled 53x41 image, not the original
-#
 
 # import and preprocess the data set
 # set the data path and import the data
@@ -44,20 +43,20 @@ unknown_img = np.load(unknown_path)
 # convert to grayscale and visualize
 img_og = ski.color.rgb2gray(img_og)
 
+# plot original - include in report
 fig, ax = plt.subplots(1, 2, figsize=(20,10))
 ax[0].imshow(img_og, cmap = 'gray')
 ax[0].set_title("Original image")
 ax[0].set_xlabel('pixel')
 ax[0].set_ylabel('pixel')
-
 print("Original size: ", img_og.shape)
 
 # rescale the image using the code from the helper code
 # resize image
 img = ski.transform.rescale(img_og, 0.18, anti_aliasing=False)
 
+# plot rescaled - include in report
 print("Rescaled size: ", img.shape)
-
 ax[1].imshow(img, cmap='gray')
 ax[1].set_title("Rescaled image")
 ax[1].set_xlabel('pixel')
@@ -67,6 +66,7 @@ ax[1].set_ylabel('pixel')
 numRows = img.shape[0]
 numCols = img.shape[1]
 
+# calculate how many pixels are in the image
 pixels = numRows * numCols
 
 
@@ -85,7 +85,6 @@ def construct_DCT_Mat(Nx, Ny):
 
     return D
 
-
 # construct inverse DCT matrix
 def construct_iDCT_Mat(Nx, Ny):
     # input : Nx number of columns of image
@@ -102,9 +101,11 @@ def construct_iDCT_Mat(Nx, Ny):
 newDCT = construct_DCT_Mat(numCols, numRows)
 newiDCT = construct_iDCT_Mat(numCols, numRows)
 
+# create a flattened vector of the image for multiplication purposes
 f = np.ndarray.flatten(img)
 index = np.arange(0, pixels)
 
+# multiply DCT and flattened image to get the DCT coefficients (DCT_F)
 DCT_F = np.matmul(newDCT, f)
 
 # UNCOMMENT THIS FOR REPORT
@@ -119,21 +120,19 @@ DCT_F = np.matmul(newDCT, f)
 # 1c) reconstruct and plot the image after thresholding its DCT to keep the top 5,10,20, and 40 percent of DCT
 # coefficients
 
-DCT_F_copy = np.copy(DCT_F)
+DCT_F_copy = np.copy(DCT_F) # make a copy bc python is weird (miss you c++)
 DCT_F_abs = np.abs(DCT_F_copy)
 DCT_F_sort = DCT_F_copy[np.argsort(-DCT_F_copy)]
 
-percentages = [0.05, 0.1, 0.2, 0.4]  # take top 5%, 10%, 20%, and 40%
+percentages = [0.05, 0.1, 0.2, 0.4]  # take top 5%, 10%, 20%, and 40% of the DCT coefficients
 
+# making some more copies so things don't get overwritten
 dctF5 = np.copy(DCT_F)
 dctF10 = np.copy(DCT_F)
 dctF20 = np.copy(DCT_F)
 dctF40 = np.copy(DCT_F)
 
-# plt.figure()
-# fig, ax = plt.subplots(2, 2, figsize=(8, 8))
-
-# too frustrated to make the for-loop work
+# too frustrated to make the for-loop work -- see code below copy and pasted several times
 # for i in range(0, len(percentages)):
 #   file = 'dctF' + str(int(percentages[i] *100))
 #   percent = round(percentages[i] * len(DCT_F_sort))
@@ -162,19 +161,18 @@ dctF40 = np.copy(DCT_F)
 # fig.suptitle(title, fontsize=24)
 
 # for 5%
-percent = round(0.05 * len(DCT_F_sort))
+percent = round(0.05 * len(DCT_F_sort)) # how many values to include
 print('For ' + str(0.05) + ' there are ' + str(percent) + ' values to include')
-threshold = DCT_F_sort[percent]
-# testing = np.percentile(DCT_F_sort)
-# fivePercent = np.zeros(pixels)
-# fivePercent[0:percent] = DCT_F_sort[0:percent]
+threshold = DCT_F_sort[percent] # the actual value at that index - keep everything above it
+# set anything less than the threshold value to 0 - WRITE to ORIGINAL vector, not sorted one
 for j in range(0, len(DCT_F)):
     if abs(dctF5[j]) < threshold:
         dctF5[j] = 0
     else:
         dctF5[j] = dctF5[j]
+# multiply that vector by the inverse DCT to get the vector for image reconstruction
 reconstruction = np.dot(newiDCT, dctF5)
-shaped = np.reshape(reconstruction, (53, 41))
+shaped = np.reshape(reconstruction, (53, 41)) # reshape so we can plot 2D image
 
 plt.figure(2)
 plt.imshow(shaped, cmap='gray')
@@ -186,17 +184,12 @@ plt.ylabel('pixel')
 percent = round(0.1 * len(DCT_F_sort))
 print('For ' + str(0.1) + ' there are ' + str(percent) + ' values to include')
 threshold = DCT_F_sort[percent]
-# testing = np.percentile(DCT_F_sort)
-# fivePercent = np.zeros(pixels)
-# fivePercent[0:percent] = DCT_F_sort[0:percent]
 for j in range(0, len(DCT_F)):
     if abs(dctF10[j]) < threshold:
         dctF10[j] = 0
     else:
         dctF10[j] = dctF10[j]
 reconstruction = np.dot(newiDCT, dctF10)
-# reconstruction = np.dot(newiDCT,fivePercent)
-# reconstruction = np.dot(DCT_F_sort,newiDCT)
 shaped = np.reshape(reconstruction, (53, 41))
 
 plt.figure(3)
@@ -205,24 +198,16 @@ plt.title(str(0.1 * 100) + "% of coefficients")
 plt.xlabel('pixel')
 plt.ylabel('pixel')
 
-
-
-
 # for 20%
 percent = round(0.2 * len(DCT_F_sort))
 print('For ' + str(0.2) + ' there are ' + str(percent) + ' values to include')
 threshold = DCT_F_sort[percent]
-# testing = np.percentile(DCT_F_sort)
-# fivePercent = np.zeros(pixels)
-# fivePercent[0:percent] = DCT_F_sort[0:percent]
 for j in range(0, len(DCT_F)):
     if abs(dctF20[j]) < threshold:
         dctF20[j] = 0
     else:
         dctF20[j] = dctF20[j]
 reconstruction = np.dot(newiDCT, dctF20)
-# reconstruction = np.dot(newiDCT,fivePercent)
-# reconstruction = np.dot(DCT_F_sort,newiDCT)
 shaped = np.reshape(reconstruction, (53, 41))
 
 plt.figure(4)
@@ -231,22 +216,16 @@ plt.title(str(0.2 * 100) + "% of coefficients")
 plt.xlabel('pixel')
 plt.ylabel('pixel')
 
-
 # for 40%
 percent = round(0.4 * len(DCT_F_sort))
 print('For ' + str(0.4) + ' there are ' + str(percent) + ' values to include')
 threshold = DCT_F_sort[percent]
-# testing = np.percentile(DCT_F_sort)
-# fivePercent = np.zeros(pixels)
-# fivePercent[0:percent] = DCT_F_sort[0:percent]
 for j in range(0, len(DCT_F)):
     if abs(dctF40[j]) < threshold:
         dctF40[j] = 0
     else:
         dctF40[j] = dctF40[j]
 reconstruction = np.dot(newiDCT, dctF40)
-# reconstruction = np.dot(newiDCT,fivePercent)
-# reconstruction = np.dot(DCT_F_sort,newiDCT)
 shaped = np.reshape(reconstruction, (53, 41))
 
 plt.figure(5)
@@ -265,14 +244,14 @@ plt.ylabel('pixel')
 # 2a) Let M < N be an integer and construct a measurement matrix B of size MxN by randomly selecting M rows of the
 # identity matrix I size NxN
 
-# make a for-loop so it calculates this for three trials
+# make a for-loop so it calculates this for three trials at each r value
 fig, ax = plt.subplots(3, 3, figsize=(6,9))
-plt.tight_layout()
-#
-trial = [1,2,3]
-M_values = [0.2, 0.4, 0.6]
-for j in range (0, 1): #len(M_values)):
-    for i in range(0,1):# len(trial)):
+plt.tight_layout() # make the plots fit nicely
+
+trial = [1,2,3] # number of trials
+M_values = [0.2, 0.4, 0.6] # r values to be tested (not named well)
+for j in range (0, len(M_values)):
+    for i in range(0, len(trial)):
         # take random permutation of N
         # then take the first M entries
         N = pixels
@@ -283,21 +262,20 @@ for j in range (0, 1): #len(M_values)):
         B_total = np.random.permutation(I)  # MxN
         B = B_total[0:M, :]
 
-        # f_vals = our flattened image
-        # y = B dot image_flat,
         y = np.dot(B, f)  # multiply by the flattened image
-        # A = B dot iDCT
+
         A = np.dot(B, newiDCT)  # multipy by the inverse DCT to reconstruct, this creates the corrupt image
 
         # cvx optimization solving
         # x is CLEARLY the DCT vector of an image F* that hopefully resembles the original image
-        x_l1 = cvx.Variable(N)
+        x_l1 = cvx.Variable(N) # create an empty variable to write to (this contains NOTHING until solver is run)
 
-        # alt formulation
+        # cvx solver formulation
         objective_l1 = cvx.Minimize(cvx.norm(x_l1, 1))
-        constraints_l1 = [A @ x_l1 == y]  # constaint in cvs = A @ x == y
+        constraints_l1 = [A @ x_l1 == y]  # constaint in cvx : A @ x == y
         prob_l1 = cvx.Problem(objective_l1, constraints_l1)
 
+        # run the solver - this calculates the solution
         prob_l1.solve(verbose=True, solver='CVXOPT', max_iter=1000, reltol=1e-2, featol=1e-2)
 
         # result should be Nx1
@@ -317,52 +295,39 @@ for j in range (0, 1): #len(M_values)):
 
 
 # TASK 3 #############################################################################################################
-# unknown_y = unknown_img.f.y
-# unknown_B = unknown_img.f.B
-# unknown_iDCT = construct_iDCT_Mat(50, 50)
-# unknown_pixels = 50*50
-#
-#
-# unk_N = unknown_pixels
-# #r = 0.6 # this will be what we iterate for 0.2, 0.4, and 0.6
-# #M = int(r * unk_N)
-#
-# # I = np.identity(pixels)  # NxN
-# # B_total = np.random.permutation(I)  # MxN
-# # B = B_total[0:M, :]
-#
-# # f_vals = our flattened image
-# # y = B dot image_flat,
-#
-#
-# #y = np.dot(unknown_B, unknown_flat)  # multiply by the flattened image
-# # A = B dot iDCT
-# A = np.dot(unknown_B, unknown_iDCT)  # multipy by the inverse DCT to reconstruct
-#
-# # cvx optimization solving
-# # x is CLEARLY the DCT vector of an image F* that hopefully resembles the original image
-# x_l1 = cvx.Variable(unk_N)
-#
-# # alt formulation
-# objective_l1 = cvx.Minimize(cvx.norm(x_l1, 1))
-# constraints_l1 = [A @ x_l1 == unknown_y]  # constaint in cvs = A @ x == y
-# prob_l1 = cvx.Problem(objective_l1, constraints_l1)
-#
-# prob_l1.solve(verbose=True, solver='CVXOPT', max_iter=1000, reltol=1e-2, featol=1e-2)
-#
-# # result should be Nx1
-# result = x_l1.value
-#
-# # then do the iDCT and reshape it back to the original image like we did in task 1
-# d_reconstruction = np.dot(unknown_iDCT, result)
-# d_shaped = np.reshape(d_reconstruction, (50, 50))
-#
-#
-# plt.figure(100)
-# plt.imshow(d_shaped,cmap='PiYG')
-# plt.title("Unknown image")
-# plt.xlabel('pixel')
-# plt.ylabel('pixel')
+unknown_y = unknown_img.f.y
+unknown_B = unknown_img.f.B
+unknown_iDCT = construct_iDCT_Mat(50, 50)
+unknown_pixels = 50*50
+
+unk_N = unknown_pixels
+
+A = np.dot(unknown_B, unknown_iDCT)  # multipy by the inverse DCT to reconstruct
+
+# cvx optimization solving
+# x is CLEARLY the DCT vector of an image F* that hopefully resembles the original image
+x_l1 = cvx.Variable(unk_N)
+
+# alt formulation
+objective_l1 = cvx.Minimize(cvx.norm(x_l1, 1))
+constraints_l1 = [A @ x_l1 == unknown_y]  # constaint in cvs = A @ x == y
+prob_l1 = cvx.Problem(objective_l1, constraints_l1)
+
+prob_l1.solve(verbose=True, solver='CVXOPT', max_iter=1000, reltol=1e-2, featol=1e-2)
+
+# result should be Nx1
+result = x_l1.value
+
+# then do the iDCT and reshape it back to the original image like we did in task 1
+d_reconstruction = np.dot(unknown_iDCT, result)
+d_shaped = np.reshape(d_reconstruction, (50, 50))
+
+
+plt.figure(100)
+plt.imshow(d_shaped,cmap='PiYG')
+plt.title("Unknown image")
+plt.xlabel('pixel')
+plt.ylabel('pixel')
 
 
 
